e59039e66bbdc4c80614f65aa35a4f9a6e6ae7e0
Itziar Zabaleta <itziar_z@hotmail.com>
2022-01-18 17:06:56 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,15 +1,16 @@
 function main()
-%   main() generates a image with added texture, according to the selected parameters
+%   main() generates an image with added texture, according to the selected parameters.
 %
 %   References:
-%   I. Zabaleta and M. Bertalmio, "Photorealistic Style Transfer for 
-%   Video" % In Signal Processing: Image Communication, 2021.
+%   Zabaleta, I., Cámara, M., Díaz, C., Canham, T., García, N., and Bertalmío, M. "Retinal Noise Emulation: 
+%   A Novel Artistic Tool for Cinema That Also Improves Compression Efficiency. In IEEE Access, 8, 67263-67276, 2020.
 % 
 %   This code is property of Universitat Pompeu Fabra. 
 
 img=imread('img\img.jpg');
 
 % Default parameters
+
 
 result = retinalNoise(img, );
 
