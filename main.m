13e637dcfcc36ccbc4b1de742a312bb2ca33bcf9
Itziar Zabaleta <itziar_z@hotmail.com>
2022-01-18 17:03:25 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,1 +1,16 @@
+function main()
+%   main() generates a image with added texture, according to the selected parameters
+%
+%   References:
+%   I. Zabaleta and M. Bertalmio, "Photorealistic Style Transfer for 
+%   Video" % In Signal Processing: Image Communication, 2021.
+% 
+%   This code is property of Universitat Pompeu Fabra. 
 
+img=imread('img\img.jpg');
+
+% Default parameters
+
+result = retinalNoise(img, );
+
+imwrite(result, 'img/results/result.jpg', 'Quality', 100);
